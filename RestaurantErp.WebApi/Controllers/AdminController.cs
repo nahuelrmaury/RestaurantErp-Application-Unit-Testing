4b859c7f7d88dbfc8834bee833bdf457dc153f46
serhii.mykhailov <moderya7@gmail.com>
2023-02-23 20:05:32 +0200

    add api

---
+++ RestaurantErp.WebApi/Controllers/AdminController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestaurantErp.Core.Contracts;
+using RestaurantErp.Core.Models.Discount;
+using RestaurantErp.Core.Models.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantErp.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AdminController : ControllerBase
+    {
+        public readonly IProductProvider _productProvider;
+        public readonly IDiscountByTimeProvider _discountByTimeProvider;
+
+        public AdminController(IProductProvider productProvider,
+            IDiscountByTimeProvider discountByTimeProvider)
+        {
+            _productProvider = productProvider;
+            _discountByTimeProvider = discountByTimeProvider;
+        }
+
+        [HttpPost("CreateProduct")]
+        public async Task<IActionResult> CreateProduct(AddProductRequest productInfo)
+        {
+            var orderId = _productProvider.AddProduct(productInfo);
+
+            return new ObjectResult(orderId);
+        }
+
+        [HttpPost("AddDiscountByTimeSettings")]
+        public async Task<IActionResult> AddDiscountByTimeSettings(DiscountByTimeSettings settings)
+        {
+            var discountId = _discountByTimeProvider.Add(settings);
+
+            return new ObjectResult(discountId);
+        }
+    }
+}
---
+++ RestaurantErp.WebApi/Startup.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using RestaurantErp.Core.Contracts;
+using RestaurantErp.Core.Helpers;
+using RestaurantErp.Core.Models;
+using RestaurantErp.Core.Models.Discount;
+using RestaurantErp.Core.Providers;
+using RestaurantErp.Core.Providers.Discount;
+using System;
+using System.Globalization;
+
+namespace RestaurantErp.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "RestaurantErp.WebApi", Version = "v1" });
+            });
+
+            var productProvider = new ProductProvider();
+
+            services.AddSingleton<IDiscountByTimeProvider, DiscountByTimeProvider>()
+                .AddSingleton<IDiscountProvider, DiscountByTimeProvider>()
+                .AddSingleton<IDiscountCalculator, DiscountCalculator>()
+                .AddSingleton<IOrderProvider, OrderProvider>()
+                .AddSingleton<IServiceChargeProvider, ServiceChargeProvider>()
+                .AddSingleton<ITimeHelper, TimeHelper>()
+                .AddSingleton<BillHelper>()
+
+                //.AddSingleton<IPriceStorage, ProductProvider>()
+                //.AddSingleton<IProductProvider>(p => p.GetService<IPriceStorage>())
+
+                .AddSingleton<ProductProvider>()
+                .AddSingleton<IPriceStorage>(p => p.GetRequiredService<ProductProvider>())
+                .AddSingleton<IProductProvider>(p => p.GetRequiredService<ProductProvider>())
+
+                .AddSingleton((p => new DiscountCalculatorSettings
+                {
+                    MinimalProductPrice = decimal.Parse(p.GetService<IConfiguration>()["MinimalProductPrice"])
+                }))
+
+                .AddSingleton((p => new DiscountByTimeProviderSettings
+                {
+                    EndDiscountDelay = TimeSpan.FromSeconds(double.Parse(p.GetService<IConfiguration>()["EndDiscountDelaySeconds"]))
+                }))
+
+                .AddSingleton((p => new ServiceChargeProviderSettings
+                {
+                    ServiceRate = decimal.Parse(p.GetService<IConfiguration>()["ServiceChargeRate"])
+                }));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            var cultureInfo = new CultureInfo("en-US");
+            cultureInfo.NumberFormat.CurrencySymbol = "£";
+
+            CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+            CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "UserService v1"));
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.Use(async (context, next) =>
+            {
+                context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+                await next.Invoke();
+            });
+
+            app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true)
+                .AllowCredentials());
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
